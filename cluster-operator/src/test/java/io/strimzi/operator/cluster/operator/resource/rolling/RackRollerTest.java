f53d21b4e9a3f54190562282f88eac8788731b5f
ShubhamRwt <shubhamrwt02@gmail.com>
2023-06-21 13:32:12 +0530

    Changes
    Signed-off-by: ShubhamRwt <shubhamrwt02@gmail.com>

--- cluster-operator/src/test/java/io/strimzi/operator/cluster/operator/resource/rolling/RackRollerTest.java
+++ cluster-operator/src/test/java/io/strimzi/operator/cluster/operator/resource/rolling/RackRollerTest.java
@@ -13,75 +13,27 @@ import java.util.function.Function;
 import java.util.stream.Stream;
 
 import static java.util.Arrays.asList;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 public class RackRollerTest {
 
 
     @Test
     public void dosuccessfulRolling() throws ExecutionException, InterruptedException, TimeoutException {
-        RollClient client = mock(MockRollClient.class);
-        RackRolling.rollingRestart(client, asList(0,1,2), new Function<Integer, Set<RestartReason>>() {
+        RollClient client = mock(RollClient.class);
+
+        when(client.isNotReady(0)).thenReturn(false);
+
+        //Assuming the pods is Serving
+        doReturn(3).when(client).getBrokerState(0);
+        doReturn(State.SERVING).when(client).observe(0);
+        doReturn(0).when(client).tryElectAllPreferredLeaders(0);
+        doReturn(0).when(client).activeController();
+        RackRolling.rollingRestart(client, asList(0), new Function<Integer, Set<RestartReason>>() {
             @Override
             public Set<RestartReason> apply(Integer integer) {
                 return Collections.emptySet();
             }
-        }, 10, 10, 2, 0);
-
-
-    }
-
-    static class MockRollClient implements RollClient {
-
-        private final static int ADMIN_BATCH_SIZE = 200;
-
-        @Override
-        public boolean isNotReady(Integer nodeId) {
-            return false;
-        }
-
-        @Override
-        public State observe(int serverId) {
-            return RollClient.super.observe(serverId);
-        }
-
-        // Assumed broker state is serving for all the pods
-        @Override
-        public int getBrokerState(Integer nodeId) {
-            return 3;
-        }
-
-        @Override
-        public void deletePod(String podName) {
-        }
-
-        @Override
-        public Collection<TopicListing> listTopics() throws ExecutionException, InterruptedException {
-            return Collections.emptyList();
-        }
-
-        @Override
-        public Stream<TopicDescription> describeTopics(List<Uuid> topicIds) throws InterruptedException, ExecutionException {
-            return Stream.empty();
-        }
-
-        @Override
-        public Map<String, Integer> describeTopicMinIsrs(List<String> topicNames) throws InterruptedException, ExecutionException {
-            return Map.of();
-        }
-
-        @Override
-        public int activeController() throws InterruptedException, ExecutionException {
-            return 1;
-        }
-
-        @Override
-        public void reconfigureServer(int serverId) {
-        }
-
-        @Override
-        public int tryElectAllPreferredLeaders(int serverId) {
-            return 0;
-        }
+        }, 1000, 1000, 1, 0);
     }
 }
