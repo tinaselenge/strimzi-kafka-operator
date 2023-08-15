ca14b289b499ce6b6c5fdb8196312ca0b7e254af
Tom Bentley <tbentley@redhat.com>
2023-08-15 17:27:09 +0100

    Oops
    Signed-off-by: Tom Bentley <tbentley@redhat.com>

--- cluster-operator/src/test/java/io/strimzi/operator/cluster/operator/resource/rolling/RackRollingTest.java
+++ cluster-operator/src/test/java/io/strimzi/operator/cluster/operator/resource/rolling/RackRollingTest.java
@@ -32,7 +32,6 @@ import java.util.function.Function;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.doAnswer;
